eb7066b02c5c47e3447aa6ed2f0b0445d0a42958
Mateusz Minder <mateusz.minder99@gmail.com>
2023-11-21 19:37:37 +0100

    changed home to candles

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import { Route, Routes } from 'react-router-dom'
 import './App.css'
-import { HomePage } from './pages/HomePage'
+import { CandlesPage } from './pages/CandlesPage'
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<HomePage />} />
+      <Route path="/" element={<CandlesPage />} />
     </Routes>
   )
 }
