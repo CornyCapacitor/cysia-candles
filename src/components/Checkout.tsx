3d17ca057bab294a38a3adb6d7bc7d7a73c243ff
Mateusz Minder <mateusz.minder99@gmail.com>
2023-11-28 23:23:23 +0100

    renamed checkout section to customer section

--- src/components/Checkout.tsx
+++ src/components/Checkout.tsx
@@ -218,8 +218,8 @@ export const Checkout = () => {
             </div>
           </div>
         </div>
-        <div className="checkout-details">
-          <header className="checkout-header">Customer information</header>
+        <div className="customer-details">
+          <header className="customer-header">Customer information</header>
           <form className="form">
             <fieldset className="fieldset">
               <div className="fieldset-section">
@@ -233,8 +233,8 @@ export const Checkout = () => {
             </fieldset>
             {customerType === "private-person" ?
               <>
-                <input placeholder="First name" className="checkout-input" type="textbox" value={customerName} onChange={(e) => setCustomerName(e.target.value)} />
-                <input placeholder="Secondary name" className="checkout-input" type="textbox" value={customerSecondName} onChange={(e) => setCustomerSecondName(e.target.value)} />
+                <input placeholder="First name" className="customer-input" type="textbox" value={customerName} onChange={(e) => setCustomerName(e.target.value)} />
+                <input placeholder="Secondary name" className="customer-input" type="textbox" value={customerSecondName} onChange={(e) => setCustomerSecondName(e.target.value)} />
                 <div className="fieldset-section">
                   <input className="checkbox" type="checkbox" onChange={() => setInvoice(!invoice)} checked={invoice === true} />
                   <label>I want to receive an invoice</label>
@@ -242,22 +242,22 @@ export const Checkout = () => {
               </>
               :
               <>
-                <input placeholder="Company name" className="checkout-input" type="textbox" value={companyName} onChange={(e) => setCompanyName(e.target.value)} />
-                <input placeholder="Company Identication Number" className="checkout-input" type="textbox" value={companyNumber} onChange={(e) => setCompanyNumber(e.target.value)} />
+                <input placeholder="Company name" className="customer-input" type="textbox" value={companyName} onChange={(e) => setCompanyName(e.target.value)} />
+                <input placeholder="Company Identication Number" className="customer-input" type="textbox" value={companyNumber} onChange={(e) => setCompanyNumber(e.target.value)} />
                 <span style={{ height: "25px" }}></span>
               </>}
-            <input type="textbox" placeholder="Street name" className="checkout-input" value={streetName} onChange={(e) => setStreetName(e.target.value)} />
+            <input type="textbox" placeholder="Street name" className="customer-input" value={streetName} onChange={(e) => setStreetName(e.target.value)} />
             <div className="adress-details">
-              <input type="textbox" className="smaller-checkout-input" placeholder="House number" value={houseNumber} onChange={(e) => setHouseNumber(e.target.value)} />
-              <input className="smaller-checkout-input" placeholder="Apartment/flat etc." value={apartmentValue} onChange={(e) => setApartmentValue(e.target.value)} />
-              <input className="smaller-checkout-input" placeholder="City" value={city} onChange={(e) => setCity(e.target.value)} />
-              <input className="smaller-checkout-input" placeholder="Zip code" value={zipCode} onChange={(e) => setZipCode(e.target.value)} />
+              <input type="textbox" className="smaller-customer-input" placeholder="House number" value={houseNumber} onChange={(e) => setHouseNumber(e.target.value)} />
+              <input className="smaller-customer-input" placeholder="Apartment/flat etc." value={apartmentValue} onChange={(e) => setApartmentValue(e.target.value)} />
+              <input className="smaller-customer-input" placeholder="City" value={city} onChange={(e) => setCity(e.target.value)} />
+              <input className="smaller-customer-input" placeholder="Zip code" value={zipCode} onChange={(e) => setZipCode(e.target.value)} />
             </div>
-            <input type="textbox" placeholder="Phone number*" className="checkout-input" value={phoneNumber} onChange={(e) => setPhoneNumber(e.target.value)} />
-            <input type="textbox" placeholder="E-mail*" className="checkout-input" value={email} onChange={(e) => setEmail(e.target.value)} />
+            <input type="textbox" placeholder="Phone number*" className="customer-input" value={phoneNumber} onChange={(e) => setPhoneNumber(e.target.value)} />
+            <input type="textbox" placeholder="E-mail*" className="customer-input" value={email} onChange={(e) => setEmail(e.target.value)} />
             <textarea placeholder="Comment" className="comments" value={comments} onChange={(e) => setComments(e.target.value)} />
             <span style={{ fontSize: "12px" }}>* fields marked with a star are optional</span>
-            <button className="checkout-button" onClick={(e) => handleBuy(e)}>Buy</button>
+            <button className="customer-button" onClick={(e) => handleBuy(e)}>Buy</button>
           </form>
         </div>
         <div className="summary-details">
