0357b8e42ccba0bf31374e5d2a15515215aef24b
Mateusz Minder <mateusz.minder99@gmail.com>
2023-12-11 12:12:33 +0100

    i18next install + i18n config

---
+++ src/config/i18n.ts
@@ -0,0 +1,29 @@
+import i18n from 'i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
+import HttpApi from 'i18next-http-backend';
+import { initReactI18next } from 'react-i18next';
+
+i18n
+  .use(initReactI18next)
+  .use(LanguageDetector)
+  .use(HttpApi)
+  .init({
+    debug: true,
+    supportedLngs: ['en', 'pl'],
+    fallbackLng: 'en',
+    detection: {
+      order: ['localStorage', 'cookie', 'querystring', 'sessionStorage', 'navigator', 'htmlTag', 'path', 'subdomain'],
+      caches: ['localStorage', 'cookie']
+    },
+    backend: {
+      loadPath: 'public/assets/locales/{{lng}}/translation.json'
+    },
+    react: {
+      useSuspense: false,
+    },
+    interpolation: {
+      escapeValue: false,
+    },
+  });
+
+export default i18n
