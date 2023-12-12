93f3a80c590730d0cdf02c1b1ededdea4e005df2
Mateusz Minder <mateusz.minder99@gmail.com>
2023-12-12 23:34:36 +0100

    paymentcreate.tsx translation

--- src/components/PaymentCreate.tsx
+++ src/components/PaymentCreate.tsx
@@ -1,5 +1,6 @@
 import { useAtom } from "jotai";
 import { useEffect, useState } from "react";
+import { useTranslation } from "react-i18next";
 import { Link, useNavigate } from "react-router-dom";
 import Swal from "sweetalert2";
 import { adminAtom, themeAtom } from "../atoms";
@@ -9,6 +10,8 @@ import './Admin.css';
 export const PaymentCreate = () => {
   const [theme] = useAtom(themeAtom)
   const [isAdmin] = useAtom(adminAtom)
+
+  const { t } = useTranslation();
 
   const [name, setName] = useState<string>("");
   const [price, setPrice] = useState<number>(0);
@@ -46,7 +49,7 @@ export const PaymentCreate = () => {
         iconColor: '#e71f1f',
         background: `${themeBackground}`,
         color: `${themeColor}`,
-        title: `Name can't be empty, price must be at least 0.00, availability must be either "Yes" or "No"!`,
+        title: `${t('payment_create_swal_error')}!`,
         timer: 5000,
       })
       return
@@ -63,7 +66,7 @@ export const PaymentCreate = () => {
         iconColor: 'green',
         background: `${themeBackground}`,
         color: `${themeColor}`,
-        title: "You've succesfully created a new payment method!",
+        title: `${t('payment_create_swal_success')}!`,
         showConfirmButton: true,
         confirmButtonText: "Ok",
         timer: 5000,
@@ -106,36 +109,36 @@ export const PaymentCreate = () => {
           <div className="section" style={{ borderTop: "none", paddingTop: "0px" }}>
             <div className={`section-single ${theme === "light" ? "light-toned-bg dark-font" : "dark-bg white-font"}`}>
               <div style={{ display: "flex", flexDirection: "column", alignItems: "center", gap: "5px" }}>
-                <span className={theme === "light" ? "light-var-font" : "dark-var-font"}>Name:</span>
+                <span className={theme === "light" ? "light-var-font" : "dark-var-font"}>{t('name')}:</span>
                 <input className={`admin-input ${theme === "light" ? "light-var-outline" : "dark-var-outline"}`} value={name} type="textbox" onChange={(e) => setName(e.target.value)} />
-                <span className={theme === "light" ? "light-var-font" : "dark-var-font"}>Price:</span>
+                <span className={theme === "light" ? "light-var-font" : "dark-var-font"}>{t('price')}:</span>
                 <input className={`admin-input ${theme === "light" ? "light-var-outline" : "dark-var-outline"}`} value={priceString} type="text" onChange={(e) => setPriceHandler(e.target.value)} />
-                <span className={theme === "light" ? "light-var-font" : "dark-var-font"}>Is available:</span>
+                <span className={theme === "light" ? "light-var-font" : "dark-var-font"}>{t('is_available')}:</span>
                 <select style={{ width: "100%", height: "40px" }} className={`admin-input ${theme === "light" ? "light-var-outline" : "dark-var-outline"}`} value={isAvailable} onChange={(e) => setAvailableHandler(e.target.value)}>
-                  <option value="" disabled>Choose one</option>
-                  <option value="true">Yes</option>
-                  <option value="false">No</option>
+                  <option value="" disabled>{t('choose_one')}</option>
+                  <option value="true">{t('yes')}</option>
+                  <option value="false">{t('no')}</option>
                 </select>
               </div>
             </div>
             <div className={`section-single ${theme === "light" ? "light-toned-bg dark-font" : "dark-bg white-font"}`}>
-              <header className={theme === "light" ? "light-var-font" : "dark-var-font"}>Preview</header>
-              <span className={theme === "light" ? "light-var-font" : "dark-var-font"}>Name:</span>
-              <span>{name}</span>
-              <span className={theme === "light" ? "light-var-font" : "dark-var-font"}>Price:</span>
+              <header className={theme === "light" ? "light-var-font" : "dark-var-font"}>{t('preview')}</header>
+              <span className={theme === "light" ? "light-var-font" : "dark-var-font"}>{t('name')}:</span>
+              <span>{t(`${name}`)}</span>
+              <span className={theme === "light" ? "light-var-font" : "dark-var-font"}>{t('price')}:</span>
               <span>{Number(priceString).toFixed(2)} PLN</span>
-              <span className={theme === "light" ? "light-var-font" : "dark-var-font"}>Is available:</span>
+              <span className={theme === "light" ? "light-var-font" : "dark-var-font"}>{t('is_available')}:</span>
               <span>{available ? <span style={{ color: "#3dff3d" }}>&#10003;</span> : <span style={{ color: "#ff3838" }}>&#10007;</span>}</span>
             </div>
           </div>
           <div style={{ display: "flex", gap: "20px", alignItems: "center" }}>
-            <button style={{ height: "60px" }} className={`admin-button ${theme === "light" ? "light-var-bg" : "dark-var-bg"}`} onClick={(e) => handleCreate(e)}>Add new payment method</button>
+            <button style={{ height: "60px" }} className={`admin-button ${theme === "light" ? "light-var-bg" : "dark-var-bg"}`} onClick={(e) => handleCreate(e)}>{t('add_new_payment_method')}</button>
             <Link to="/admin">
-              <button className={`admin-button ${theme === "light" ? "light-var-bg" : "dark-var-bg"}`}>Cancel creating</button>
+              <button className={`admin-button ${theme === "light" ? "light-var-bg" : "dark-var-bg"}`}>{t('cancel_creating')}</button>
             </Link>
           </div>
         </div>
-        : <>You shouldn't be here</>}
+        : <>{t('not_an_admin_warn')}</>}
     </div>
   )
 }
