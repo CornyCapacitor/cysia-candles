a7b9c37a2b9b8a37c283829233de8f3996acdf37
Mateusz Minder <mateusz.minder99@gmail.com>
2024-03-14 21:46:28 +0100

    separeted environment file for security purpose

--- src/config/supabaseClient.ts
+++ src/config/supabaseClient.ts
@@ -1,8 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabase = createClient(
-  'https://ysmgdfkjyyjgldpqhnph.supabase.co',
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlzbWdkZmtqeXlqZ2xkcHFobnBoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDA2NDkzMjIsImV4cCI6MjAxNjIyNTMyMn0.Kf_R_24HWeKgVv8bw_Xrc2KgVm4zd2IMzgv6bJZXHng',
+  import.meta.env.VITE_PUBLIC_SUPABASE_URL as string,
+  import.meta.env.VITE_PUBLIC_SUPABASE_ANON_KEY as string,
 );
 
 export default supabase;
